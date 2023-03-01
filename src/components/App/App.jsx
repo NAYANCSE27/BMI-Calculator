c23cf0af6fa872ec63e1797f35524f4be450df5a
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-03-02 00:28:04 +0600

    added file but map function did not work properly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import 'materialize-css/dist/css/materialize.min.css';
-import './App.css';
 import BmiForm from '../BmiForm/BmiForm';
 import Info from '../Info/Info';
 import Bar from '../Bar/Bar';
 import { getData, storeData } from '../../helpers/localStorage';
+
+import './App.css';
 
 const App = () => {
 	const initialState = () => getData('data') || [];
@@ -45,46 +46,51 @@ const App = () => {
 	return (
 		<div className="container">
 			<div className="row center">
-				<h1 className="white-text">BMI Tracker</h1>
+				<h1 className="white-text"> BMI Tracker </h1>
 			</div>
 			<div className="row">
 				<div className="col m12 s12">
-					<BmiForm chage={handleChange} />
+					<BmiForm change={handleChange} />
 					<Bar labelData={data.date} bmiData={data.bmi} />
-					<div className="row center">
-						<h4 className="white-text">7 Day Data</h4>
+					<div>
+						<div className="row center">
+							<h4 className="white-text">7 Day Data</h4>
+						</div>
+						<div className="data-container row">
+							{state.length > 0 ? (
+								<>
+									{state.map((info) => (
+										<Info
+											key={info.id}
+											id={info.id}
+											weight={info.weight}
+											height={info.height}
+											date={info.date}
+											bmi={info.bmi}
+											deleteCard={handleDelete}
+										/>
+									))}
+								</>
+							) : (
+								<div className="center white-text">
+									No log found
+								</div>
+							)}
+						</div>
 					</div>
-					<div className="data-container row">
-						{state.length > 0 ? (
-							<>
-								{state.map((info) => (
-									<Info
-										key={info.id}
-										id={info.id}
-										weight={info.weight}
-										height={info.height}
-										date={info.date}
-										bmi={info.bmi}
-										deleteCard={handleDelete}
-									/>
-								))}
-							</>
-						) : (
-							<div className="center white-text">
-								No log found
-							</div>
-						)}
-					</div>
+					{getData('lastState') !== null ? (
+						<div className="center">
+							<button
+								className="calculate-btn"
+								onClick={handleUndo}
+							>
+								Undo
+							</button>
+						</div>
+					) : (
+						''
+					)}
 				</div>
-				{getData('lastState') !== null ? (
-					<div className="center">
-						<button className="calculate-btn" onClick={handleUndo}>
-							Undo
-						</button>
-					</div>
-				) : (
-					''
-				)}
 			</div>
 		</div>
 	);
